a314262352b2f1518c8d6d7d0f2bb56922674842
Christian D. Lamothe <c.lamothe25@gmail.com>
2017-11-04 01:37:49 +0000

    initial commit

---
+++ initials.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    //get string from user
+    printf("Name: ");
+    string s = get_string();
+
+    //iterate through the string
+    for (int i = 0; i < strlen(s); i++)
+    {
+        //if 0'th character is a letter..
+        if (i == 0 && isalpha(s[0]))
+        {
+            //print 0'th character to uppercase
+            printf("%c", toupper(s[0]));
+        }
+        //if i'th character is a space and the next character is a letter..
+        else if (s[i] == ' ' && isalpha(s[i + 1]))
+        {
+            //print character after the space to uppercase
+            printf("%c", toupper(s[i + 1]));
+        }
+    }
+    printf("\n");
+}
