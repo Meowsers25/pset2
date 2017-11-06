6a14a26c1d47c880faa9a6f78ef263a07958d5e0
Christian D. Lamothe <c.lamothe25@gmail.com>
2017-11-06 04:06:02 +0000

    almost......

--- caesar2.c
+++ caesar2.c
@@ -10,11 +10,32 @@ int main(int argc, string argv[])
     {
         return 1;
     }
-    int i = atoi(argv[1]);
-    printf("integer is %i\n", i);
+    int x = atoi(argv[1]);
+    printf("integer is %i\n", x);
 
     printf("plaintext: ");
     string ptext = get_string();
     printf("text is: %s\n", ptext);
 
+    for (int i = 0, n = strlen(ptext); i < n; i++)
+    {
+        if (ptext[i] == ' ')
+        {
+            printf("%c", ptext[i]);
+        }
+        else if (isalpha(ptext[i]))
+        {
+            if (isupper(ptext[i]))
+            //if (isupper(ptext[i]) || islower(ptext[i]))
+            {
+                printf("%c", (ptext[i] - 65 + x) % 26 + 65);
+            }
+             if (islower(ptext[i]))
+            {
+                printf("%c", (ptext[i] - 97 + x) % 26 + 97);
+            }
+
+        }
+    }
+    printf("\n");
 }
